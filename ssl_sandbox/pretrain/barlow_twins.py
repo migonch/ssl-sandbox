3e2cbf4e3f87d67261340e374d4c1f65ba4a3726
mgoncharov <m.goncharov@ira-labs.com>
2023-08-24 21:13:43 +0300

    barlow twins ood

--- ssl_sandbox/pretrain/barlow_twins.py
+++ ssl_sandbox/pretrain/barlow_twins.py
@@ -1,14 +1,13 @@
-from typing import Any
-from sklearn.metrics import roc_auc_score
-import numpy as np
+from typing import Any, Literal
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 import pytorch_lightning as pl
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+from torchmetrics import AUROC, MeanMetric
 
+from ssl_sandbox.nn.resnet import resnet50, adapt_to_cifar10
 from ssl_sandbox.nn.blocks import MLP
 from ssl_sandbox.nn.functional import off_diagonal, eval_mode
 
@@ -16,11 +15,13 @@ from ssl_sandbox.nn.functional import off_diagonal, eval_mode
 class BarlowTwins(pl.LightningModule):
     def __init__(
             self,
-            encoder: nn.Module,
-            embed_dim: int,
+            encoder_architeture: Literal['resnet50', 'resnet50_cifar10'],
+            dropout_rate: float = 0.5,
+            drop_channel_rate: float = 0.5,
+            drop_block_rate: float = 0.0,
+            drop_path_rate: float = 0.1,
             proj_dim: int = 8192,
             lmbd: float = 5e-3,
-            unbiased: bool = False,
             lr: float = 1e-2,
             weight_decay: float = 1e-6,
             warmup_epochs: int = 10,
@@ -28,49 +29,54 @@ class BarlowTwins(pl.LightningModule):
     ):
         super().__init__()
 
-        self.save_hyperparameters(ignore='encoder')
+        if encoder_architeture in ['resnet50', 'resnet50_cifar10']:
+            encoder = resnet50(
+                drop_channel_rate=drop_channel_rate,
+                drop_block_rate=drop_block_rate,
+                drop_path_rate=drop_path_rate
+            )
+            encoder.fc = nn.Identity()
+            embed_dim = 2048
+            if encoder_architeture == 'resnet50_cifar10':
+                encoder = adapt_to_cifar10(encoder)
+        else:
+            raise ValueError(f'``encoder={encoder}`` is not supported')
 
         self.encoder = encoder
+        self.embed_dim = embed_dim
         self.projector = nn.Sequential(
-            MLP(embed_dim, proj_dim, proj_dim, num_hidden_layers=2, bias=False),
+            MLP(embed_dim, proj_dim, proj_dim, num_hidden_layers=2, dropout_rate=dropout_rate, bias=False),
             nn.BatchNorm1d(proj_dim, affine=False)
         )
 
         self.lmbd = lmbd
-        self.unbiased = unbiased
         self.lr = lr
         self.weight_decay = weight_decay
         self.warmup_epochs = warmup_epochs
+
+        self.save_hyperparameters()
 
     def forward(self, images):
         return self.encoder(images)
 
     def training_step(self, batch, batch_idx):
         (_, views_1, views_2), _ = batch
+        views = torch.cat((views_1, views_2))
 
-        embeds_1 = self.projector(self.encoder(views_1))  # (batch_size, proj_dim)
-        embeds_2 = self.projector(self.encoder(views_2))  # (batch_size, proj_dim)
-        
+        embeds = self.projector(self.encoder(views))
+        embeds_1, embeds_2 = torch.chunk(embeds, 2)
+
         n, d = embeds_1.shape
-        if not self.unbiased:
-            c = embeds_1.T @ embeds_2 / (n - 1)  # (proj_dim, proj_dim)
-            on_diag = c.diagonal().add_(-1).pow_(2).mean()
-            off_diag = off_diagonal(c).pow_(2).sum().div(d)
-        else:
-            assert n >= 4
-            c_1 = embeds_1[:n // 2].T @ embeds_2[:n // 2] / (n // 2 - 1)
-            c_2 = embeds_1[n // 2:].T @ embeds_2[n // 2:] / (n // 2 - 1)
-            on_diag = torch.mean(c_1.diagonal().add_(-1) * c_2.diagonal().add_(-1))
-            off_diag = torch.sum(off_diagonal(c_1) * off_diagonal(c_2)).div(d)
+        c = embeds_1.T @ embeds_2 / n  # (proj_dim, proj_dim)
+        c = self.all_gather(c, sync_grads=True).mean(dim=0)
+        on_diag = c.diagonal().add_(-1).pow_(2).mean()
+        off_diag = off_diagonal(c).pow_(2).sum().div(d)
 
         loss = on_diag + self.lmbd * off_diag
-        
-        mse_loss = F.mse_loss(embeds_1, embeds_2)
 
-        self.log(f'train/on_diag', on_diag, on_epoch=True)
-        self.log(f'train/off_diag', off_diag, on_epoch=True)
-        self.log(f'train/mse_loss', mse_loss, on_epoch=True)
-        self.log(f'train/loss', loss, on_epoch=True)
+        self.log(f'train/on_diag', on_diag, on_epoch=True, sync_dist=True)
+        self.log(f'train/off_diag', off_diag, on_epoch=True, sync_dist=True)
+        self.log(f'train/loss', loss, on_epoch=True, sync_dist=True)
 
         return loss
 
@@ -87,32 +93,33 @@ class BarlowTwins(pl.LightningModule):
 
 
 class BarlowTwinsOODDetection(pl.Callback):
-    def on_validation_epoch_start(self, trainer, pl_module) -> None:
-        self.ood_labels = []
-        self.ood_scores = []
-        self.md_scores = []
+    def __init__(self):
+        super().__init__()
+
+        self.ood_auroc = AUROC('binary')
+        self.avg_ood_score_for_ood_data = MeanMetric()
+        self.avg_ood_score_for_id_data = MeanMetric()
+
+    def on_fit_start(self, trainer, pl_module):
+        self.ood_auroc.to(pl_module.device)
+        self.avg_ood_score_for_ood_data.to(pl_module.device)
+        self.avg_ood_score_for_id_data.to(pl_module.device)
 
     def on_validation_batch_end(self, trainer, pl_module: BarlowTwins, outputs, batch, batch_idx, dataloader_idx=0):
-        (images, *views), labels = batch
-        ood_labels = labels.cpu() == -1
+        (_, *views), labels = batch
+        ood_labels = labels == -1
 
-        with eval_mode(pl_module.encoder, enable_dropout=True), eval_mode(pl_module.projector):
-            embeds = torch.stack([pl_module.projector(pl_module.encoder(v)) for v in views]).detach().cpu()
+        with eval_mode(pl_module, enable_dropout=True):
+            embeds = torch.stack([pl_module.projector(pl_module.encoder(v)) for v in views])
             ood_scores = embeds.var(0).mean(-1)
-
-        with eval_mode(pl_module.encoder), eval_mode(pl_module.projector):
-            embeds = pl_module.projector(pl_module.encoder(images)).detach().cpu()
-            md_scores = embeds.pow_(2).sum(-1)
-
-        self.ood_labels.extend(ood_labels.tolist())
-        self.ood_scores.extend(ood_scores.tolist())
-        self.md_scores.extend(md_scores.tolist())
+            self.ood_auroc.update(ood_scores, ood_labels)
+            self.avg_ood_score_for_ood_data.update(ood_scores[ood_labels])
+            self.avg_ood_score_for_id_data.update(ood_scores[~ood_labels])
 
     def on_validation_epoch_end(self, trainer, pl_module) -> None:
-        self.log('val/ood_auroc', roc_auc_score(self.ood_labels, self.ood_scores))
-        self.log('val/ood_auroc_md', roc_auc_score(self.ood_labels, self.md_scores))
-
-        ood_scores = np.array(self.ood_scores)
-        ood_labels = np.array(self.ood_labels)
-        self.log('val/mean_ood_score_for_ood_data', ood_scores[ood_labels].mean())
-        self.log('val/mean_ood_score_for_id_data', ood_scores[~ood_labels].mean())
+        self.log('val/ood_auroc', self.ood_auroc.compute(), sync_dist=True)
+        self.ood_auroc.reset()
+        self.log(f'val/avg_ood_score_for_ood_data', self.avg_ood_score_for_ood_data.compute(), sync_dist=True)
+        self.avg_ood_score_for_ood_data.reset()
+        self.log(f'val/avg_ood_score_for_id_data', self.avg_ood_score_for_id_data.compute(), sync_dist=True)
+        self.avg_ood_score_for_id_data.reset()
